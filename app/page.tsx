99bb1bef2182441b7e019e8028be8331d3b6e8d6
abhraneeldhar <abhraneeldhar@gmail.com>
2025-01-30 00:53:32 +0530

    added logo

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,18 @@
 import Image from "next/image"
+import logo from "../public/lifecatchlogo.png"
 export default function Home() {
   return (<>
     <div className="mainHome">
       <div className="navBar">
         <div className="appLogo">
-          
+          <Image src={logo} height={30} width={30} alt=""/>
         </div>
         <div className="tags">
-
+          <a href="#lifecatch">Life Catch</a>
+          <a href="#ourtarget">Target</a>
+          <a href="#subscribe">Subscribe</a>
+          <a href="#prototype">Prototype</a>
+          <a href="#aboutus">About us</a>
         </div>
       </div>
 
