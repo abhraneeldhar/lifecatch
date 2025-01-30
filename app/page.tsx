c361eae7562a765e3b34875d5ef5eddc4625c4b5
abhraneeldhar <abhraneeldhar@gmail.com>
2025-01-30 21:33:36 +0530

    lil tweak

--- app/page.tsx
+++ app/page.tsx
@@ -43,15 +43,15 @@ export default function Home() {
       <div className="teamLifeCatchSection">
         <h1 className='sectionHeading'>Who are we ?</h1>
         <div className='sectionContent'>
-          <p>We are LifeCatch, a team dedicated to redefining safety with innovative airbag technology. Our mission is simple: to protect lives by minimizing the impact of falls, whether on the road or in everyday life. Designed for both high-speed bikers and individuals with slower reflexes, our airbag jacket deploys in an instant, acting as a personal safety net when you need it most.
-
+          <p>We are LifeCatch, a team dedicated to redefining safety with innovative airbag technology. <br/> <br/>Our mission is simple: to protect lives by minimizing the impact of falls, whether on the road or in everyday life. Designed for both high-speed bikers and individuals with slower reflexes, our airbag jacket deploys in an instant, acting as a personal safety net when you need it most.
+            <br />
             At LifeCatch, we believe that safety should never be a compromise. Whether you're riding at full speed or navigating daily challenges, our technology is built to cushion falls, reduce injuries, and give you the confidence to move freely. With cutting-edge sensors and rapid deployment, our jacket ensures protection is always one step ahead.
-
-            Because every second counts, and every life matters.<br /> Stay fast, stay safe—LifeCatch has you covered.</p>
+            <br />
+            Because every second counts, and every life matters.<br /><br /> Stay fast, stay safe—LifeCatch has you covered.</p>
 
           <div className='sectionImages'>
-            <Image className='blackBikerImage ' src={blackBiker} alt=""/>
-            <Image className='adultModelImage' src={adultModel} alt=""/>
+            <Image className='blackBikerImage ' src={blackBiker} alt="" />
+            <Image className='adultModelImage' src={adultModel} alt="" />
 
           </div>
         </div>
