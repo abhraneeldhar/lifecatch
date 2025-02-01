5bbd269bbd0eef1371202f6c7f8e344b6ed828ae
abhraneeldhar <abhraneeldhar@gmail.com>
2025-02-01 18:37:58 +0530

    button position adjusted

--- app/subscribe/page.tsx
+++ app/subscribe/page.tsx
@@ -1,6 +1,16 @@
 import { Button } from "@/components/ui/button"
 import styles from "./subscribe.module.css"
 
+import kid1 from "../../public/storeImage/kid1.webp"
+import kid2 from "../../public/storeImage/kid2.webp"
+import kid3 from "../../public/storeImage/kid3.webp"
+import kid4 from "../../public/storeImage/kid4.webp"
+import old1 from "../../public/storeImage/old1.jpeg"
+import old2 from "../../public/storeImage/old2.jpeg"
+import adult1 from "../../public/storeImage/adult1.webp"
+import adult2 from "../../public/storeImage/adult2.webp"
+
+
 export default function SubscribePage() {
     return (<>
         <div className={styles.main}>
@@ -8,7 +18,9 @@ export default function SubscribePage() {
             <div className={styles.storeArea}>
                 <div className={styles.imageArea}>
                     <div className={styles.imagesTab}></div>
-                    <div className={styles.bigImage}></div>
+                    <div className={styles.bigImage}>
+                    </div>
+                        <Button className={styles.subscribeBtn}>Subscribe</Button>
                 </div>
                 <div className={styles.detailsArea}>
                     <h1>Wearable full body jacket</h1>
@@ -25,7 +37,7 @@ export default function SubscribePage() {
                         </li>
                         <li>✅ Weather-Resistant – Built to withstand different climates, keeping you safe in all conditions.</li>
                     </ul>
-                    <Button className={styles.subscribeBtn}>Subscribe</Button>
+
                 </div>
             </div>
         </div>
